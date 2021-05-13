a494afb54a4d292c1bf4ba1d355db61ee109944b
cafeaulate <kira.conh@outlook.com>
2021-05-13 23:57:00 +0900

    first comit

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  before do
+    @order = FactoryBot.build(:order)
+  end
+
+  context '内部に問題がない場合' do
+    it "priceがあれば保存できること" do
+      expect(@order).to be_valid
+    end
+  end
+
+  context '内部に問題がある場合' do
+    it "priceが空では保存できないこと" do
+      @order.price = nil
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Price can't be blank")
+    end
+  end
+end
+
+
